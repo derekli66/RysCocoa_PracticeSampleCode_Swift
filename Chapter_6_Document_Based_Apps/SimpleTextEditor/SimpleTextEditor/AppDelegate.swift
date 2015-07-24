80c0bf53ae1c45abb713a4fa961aa9473352ba30
Derek Li <derekli@zoaks.com>
2015-07-24 18:35:42 +0800

    Add new project for chapter 6

---
+++ Chapter_6_Document_Based_Apps/SimpleTextEditor/SimpleTextEditor/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SimpleTextEditor
+//
+//  Created by LEE CHIEN-MING on 7/24/15.
+//  Copyright (c) 2015 CHIENMING LEE. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
