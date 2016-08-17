3f1c992de1d9aa2a7e389e62716b987fae854233
Derek Lee <derek.lee@positivegrid.com>
2016-08-17 22:47:59 +0800

    Update ch6 and ch4.

--- Chapter_6_Document_Based_Apps/SimpleTextEditor/SimpleTextEditor/Document.swift
+++ Chapter_6_Document_Based_Apps/SimpleTextEditor/SimpleTextEditor/Document.swift
@@ -7,10 +7,13 @@
 //
 
 import Cocoa
+import AppKit
 
 class Document: NSDocument {
     
     private(set) var text: NSTextStorage  // make text be readonly
+    
+    @IBOutlet var textView: NSTextView!
 
     override init() {
         
@@ -19,10 +22,23 @@ class Document: NSDocument {
         super.init()
         // Add your subclass-specific initialization here.
     }
+    
+    convenience init(type typeName: String) throws {
+        self.init()
+        let attributedString = NSAttributedString(string: "Hello! World!")
+        self.text.appendAttributedString(attributedString)
+    }
 
     override func windowControllerDidLoadNib(aController: NSWindowController) {
         super.windowControllerDidLoadNib(aController)
         // Add any code here that needs to be executed once the windowController has loaded the document's window.
+
+        let aView: NSView = NSView(frame: NSMakeRect(20.0, 20.0, 100.0, 100.0))
+        aView.wantsLayer = true
+        aView.layer?.backgroundColor = NSColor.redColor().CGColor
+        aController.window?.contentView?.addSubview(aView)
+        textView.layoutManager?.replaceTextStorage(text)
+        
     }
 
     override class func autosavesInPlace() -> Bool {
@@ -35,21 +51,28 @@ class Document: NSDocument {
         return "Document"
     }
 
-    override func dataOfType(typeName: String, error outError: NSErrorPointer) -> NSData? {
+    override func dataOfType(typeName: String) throws -> NSData {
         // Insert code here to write your document to data of the specified type. If outError != nil, ensure that you create and set an appropriate error when returning nil.
-        // You can also choose to override fileWrapperOfType:error:, writeToURL:ofType:error:, or writeToURL:ofType:forSaveOperation:originalContentsURL:error: instead.
-        outError.memory = NSError(domain: NSOSStatusErrorDomain, code: unimpErr, userInfo: nil)
-        return nil
+        let plainText = text.string
+        var data = plainText.dataUsingEncoding(NSUTF8StringEncoding)
+        data = data ?? NSData()
+        return data!
     }
 
-    override func readFromData(data: NSData, ofType typeName: String, error outError: NSErrorPointer) -> Bool {
+    override func readFromData(data: NSData, ofType typeName: String) throws {
         // Insert code here to read your document from the given data of the specified type. If outError != nil, ensure that you create and set an appropriate error when returning false.
         // You can also choose to override readFromFileWrapper:ofType:error: or readFromURL:ofType:error: instead.
         // If you override either of these, you should also override -isEntireFileLoaded to return NO if the contents are lazily loaded.
-        outError.memory = NSError(domain: NSOSStatusErrorDomain, code: unimpErr, userInfo: nil)
-        return false
+        let plainText = String(data: data, encoding: NSUTF8StringEncoding)
+        let range = NSMakeRange(0, self.text.length)
+        self.text.replaceCharactersInRange(range, withString: plainText ?? "" )
     }
 
-
+    override func prepareSavePanel(savePanel: NSSavePanel) -> Bool {
+        savePanel.extensionHidden = false
+        savePanel.canSelectHiddenExtension = false
+        savePanel.message = "Select a destination for your text file."
+        return true
+    }
 }
 
